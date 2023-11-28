33418374c4de0cccbe1583400d819662368b64f9
unknown <mahsuma.rezai8@gmail.com>
2023-11-28 05:55:51 -0800

    add images

--- src/Component/Head/Head.js
+++ src/Component/Head/Head.js
@@ -1,18 +1,23 @@
 import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import classes from './Head.module.css'
+import classes from './Head.module.css';
+import img from '../images/logo.png'
+
 const Head = (props) => {
     return (
         <Fragment>
-            <ul className={classes.head}>
+            
+            <div className={classes.head}>
+                <img src={img} alt="" className={classes.logo} />
+
                 <li><Link to='' className={classes.link}> Home </Link></li>
                 <li><Link to="/Browse" className={classes.link}> Browse </Link></li>
-                <li><Link to="/Details"className={classes.link}> Details </Link></li>
+                <li><Link to="/Details" className={classes.link}> Details </Link></li>
                 <li><Link to="/Stre" className={classes.link}> Streams </Link></li>
                 <li><Link to="/Profile" className={classes.link}> Profile </Link></li>
 
 
-            </ul>
+            </div>
 
         </Fragment>
     )
