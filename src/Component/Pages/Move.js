559aea990091d3b2fa2905a054164024521fa210
unknown <mahsuma.rezai8@gmail.com>
2023-12-07 01:09:24 -0800

    add move section

--- src/Component/Pages/Move.js
+++ src/Component/Pages/Move.js
@@ -1,8 +1,23 @@
-import classes from './Move.module.css'
+import classes from './Move.module.css';
+import img1 from '../images/stream-01.jpg';
+import profile1 from '../images/avatar-01.jpg'
 const Move = (props) => {
     return (
         <div className={classes.move}>
             <h1 className={classes.title}>Most Popular Live Stream</h1>
+            <div>
+                <div className={classes.chart}>
+                    <img src={img1} alt="" />
+                    <div>
+                        <img alt="" src={profile1} />
+                        <p> KenganC</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+
 
         </div>
     )
