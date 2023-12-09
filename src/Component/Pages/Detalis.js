1703db89f423842151df8a0eafcf10184311a1bc
unknown <mahsuma.rezai8@gmail.com>
2023-12-09 08:16:18 -0800

    add last commit for today

--- src/Component/Pages/Detalis.js
+++ src/Component/Pages/Detalis.js
@@ -2,13 +2,17 @@ import { Fragment } from 'react'
 import Card from './Card';
 import HomeDetalis from './HomeDetalis';
 import DetalisDowlond from './DetalisDowlond';
+import LibOne from './LIbOne';
+import Libery from './Libery';
 const Detalis = (props) => {
     return (
         <Fragment>
             <Card>
                 <HomeDetalis />
                 <DetalisDowlond />
-
+                <Libery></Libery>
+                <Libery></Libery>
+                <Libery></Libery>
             </Card>
 
         </Fragment>
